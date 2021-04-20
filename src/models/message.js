9f87676e981903be03f82c27725ebe014b9837be
Ida Rosvall <ir222gn@student.lnu.se>
2021-04-20 16:02:36 +0200

    Update models to serialize their id

--- src/models/message.js
+++ src/models/message.js
@@ -21,7 +21,23 @@ const schema = new mongoose.Schema({
   }
 }, {
   timestamps: true,
-  versionKey: false
+  versionKey: false,
+  toJSON: {
+    /**
+     * Performs a transformation of the resulting object to remove sensitive information.
+     *
+     * @param {object} doc - The mongoose document which is being converted.
+     * @param {object} ret - The plain object representation which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret._id
+    },
+    virtuals: true // ensure virtual fields are serialized
+  }
+})
+
+schema.virtual('id').get(function () {
+  return this._id.toHexString()
 })
 
 // Create a model using the schema.
